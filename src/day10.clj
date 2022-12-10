81fc36df510e946774e504e7280679ddcc89499c
Mark Wardle <mark@wardle.org>
2022-12-10 16:04:20 +0000

    Add part 2

--- src/day10.clj
+++ src/day10.clj
@@ -20,6 +20,8 @@
 
 (def interesting-cycles #{20 60 100 140 180 220})
 
+(defn coords [x] (let [x' (dec x)] [(mod x' 40) (int (/ x' 40))]))
+
 (comment
   (def input (str/split-lines "noop\naddx 3\naddx -5"))
   (def input (str/split-lines "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop"))
@@ -32,5 +34,11 @@
        (map (fn [{:keys [cycle x] :as state}] (assoc state :signal-strength (* cycle x))))
        (filter (fn [{:keys [cycle]}] (interesting-cycles cycle)))
        (map :signal-strength)
-       (reduce +)))
-
+       (reduce +))
+  (->> (execute-all (mapcat parse-instruction input))
+       (map (fn [{:keys [cycle x] :as state}]
+              (assoc state :coords (coords cycle)
+                           :pixel (if (#{(dec x) x (inc x)} (first (coords cycle))) "#" "."))))
+       (map :pixel)
+       (partition 40)
+       (map #(apply str %))))
