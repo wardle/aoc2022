fd5c0f6f8ecd1cfdb28150bf4ac3a46d03109448
Mark Wardle <mark@wardle.org>
2022-12-13 11:17:07 +0000

    day 13

---
+++ src/day13.clj
@@ -0,0 +1,44 @@
+(ns day13
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.edn :as edn]))
+
+(defn grouped-lines [s]
+  (map #(str/split % #"\n") (str/split s #"\n\n")))
+(defn grouped-data [s]
+  (map (fn [lines] (map edn/read-string lines)) (grouped-lines s)))
+
+(defn compare-packet
+  "Returns a negative number, zero, or a positive number when a is logically 'less than', 'equal to', or 'greater than' b."
+  [a b]
+  (cond
+    (and (nil? a) (nil? b)) 0
+    (and (number? a) (number? b)) (compare a b)
+    (and (seqable? a) (number? b)) (compare-packet a [b])
+    (and (number? a) (seqable? b)) (compare-packet [a] b)
+    (and (seq a) (not (seq b))) 1
+    (and (not (seq a)) (seq b)) -1
+    :else (let [[a' & a-rest] a
+                [b' & b-rest] b
+                r1 (compare-packet a' b')]
+            (if (= 0 r1) (compare-packet a-rest b-rest) r1))))
+
+(comment
+  (def data "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]")
+  (def data (slurp (io/resource "day13.txt")))
+  ;; part 1
+  (->> (grouped-data data)
+       (map-indexed (fn [i [left right]] [(inc i) (compare-packet left right)]))
+       (filter (fn [[i cmp]] (>= 0 cmp)))
+       (map (fn [[i cmp]] i))
+       (reduce +))
+
+  ;; part 2
+  (->> (str/split (str data "\n[[2]]\n[[6]]\n") #"\n")
+       (remove str/blank?)
+       (map edn/read-string)
+       (sort compare-packet)
+       (map-indexed (fn [i v] [(inc i) v]))
+       (filter (fn [[i v]] (or (= [[2]] v) (= [[6]] v))))
+       (map (fn [[i v]] i))
+       (reduce *)))
